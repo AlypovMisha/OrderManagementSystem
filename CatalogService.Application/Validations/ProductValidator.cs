5f7fe83c473e8ecbd85ed37c388758e72fbeb236
AlypovMisha <144989572+AlypovMisha@users.noreply.github.com>
2025-01-06 16:20:35 +0500

    123

---
+++ CatalogService.Application/Validations/ProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using CatalogService.Application.DTOs;
+using FluentValidation;
+
+namespace CatalogService.Application.Validations
+{
+    public class ProductValidator : AbstractValidator<ProductDTO>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+            RuleFor(x => x.Category)
+                .NotEmpty().WithMessage("Category is required.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than zero.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThanOrEqualTo(1).WithMessage("Quantity must be at least 1.");
+        }
+    }
+}
--- CatalogService.Presentation/Controllers/ProductsController.cs
+++ CatalogService.Presentation/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CatalogService.Application.DTOs;
+using CatalogService.Application.Interfaces;
+using CatalogService.Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogService.Presentation.Controllers
 {
@@ -6,6 +9,35 @@ namespace CatalogService.Presentation.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly IProductService _productService;
 
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductDTO product)
+        {
+            try
+            {
+                bool isCreated = await _productService.CreateProductAsync(product);
+
+                if (isCreated)
+                {
+                    return Ok();
+                }
+
+                return StatusCode(500, "An error occurred while creating the product.");
+            }
+            catch (ExceptionNameAlreadyExists ex)
+            {
+                return Conflict(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
     }
 }
