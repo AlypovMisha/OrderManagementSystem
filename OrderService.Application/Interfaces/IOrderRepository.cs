3967e9d375f3fc7369685b4c4715e5393cbf7724
AlypovMisha <144989572+AlypovMisha@users.noreply.github.com>
2025-01-31 20:40:15 +0500

    Начал делать OrderService

---
+++ OrderService.Application/Interfaces/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Core.Entities;
+
+namespace OrderService.Application.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task CreateOrderAsync(Order order);
+        Task SaveChangesAsync();
+        Task UpdateOrderAsync(Order updateProduct);
+    }
+}
---
+++ OrderService.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using OrderService.Application.Interfaces;
+using OrderService.Core.Entities;
+
+namespace OrderService.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task CreateOrderAsync(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderAsync(Order updateProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
